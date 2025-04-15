ab6f2e765c122f62627081ec23596df5bc641d9a
birbstorm <russellandy98@gmail.com>
2025-04-15 23:17:29 +0200

    added turning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # install pygame
 import pygame
-import player as p
+from player import Player
 from constants import *
 
 
@@ -9,7 +9,7 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
-    player = p.Player(SCREEN_WIDTH /2, SCREEN_HEIGHT/2)
+    player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT/2)
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -20,11 +20,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen,(0,0,0))
+        player.update(dt)
+
+        screen.fill("black")
         player.draw(screen)
 
         pygame.display.flip()
-        dt = clock.tick()/1000
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
