ea6c68dfe7c820dfcb93aad78ac343ad6666b205
birbstorm <russellandy98@gmail.com>
2025-04-16 00:11:07 +0200

    added asteroid seperation and destruction

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,7 @@
 from circleshape import CircleShape
 from constants import *
 import pygame
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,3 +12,18 @@ class Asteroid(CircleShape):
     
     def update(self, dt):
         self.position += (self.velocity * dt)
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            newAngle = random.uniform(20,50)
+            a = self.velocity.rotate(newAngle)
+            b = self.velocity.rotate(-newAngle)
+
+            newRadius = self.radius - ASTEROID_MIN_RADIUS
+            asteroid = Asteroid(self.position.x,self.position.y,newRadius)
+            asteroid.velocity = a * 1.2
+            asteroid = Asteroid(self.position.x,self.position.y,newRadius)
+            asteroid.velocity = b * 1.2
